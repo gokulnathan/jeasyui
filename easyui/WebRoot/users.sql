41aa63f61fa952db0c802e7ed6f5ee8867df452c
sgq1980@gmail.com <sgq1980@gmail.com@4e47c4b2-cd81-ba98-324a-050b598eeac0>
2011-12-31 13:20:41 +0000

    users表创建SQL

---
+++ easyui/WebRoot/users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  firstname varchar(45) DEFAULT NULL,
+  lastname varchar(45) DEFAULT NULL,
+  phone varchar(45) DEFAULT NULL,
+  email varchar(45) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ;
+
+
