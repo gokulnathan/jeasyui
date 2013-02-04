75e769f61ba7d05ca715d954dc7cf3d419eeaa02
sgq1980@gmail.com <sgq1980@gmail.com@4e47c4b2-cd81-ba98-324a-050b598eeac0>
2013-02-04 14:36:45 +0000

    js 学习

--- trunk/easyui/WebRoot/jsdom/ADS.js
+++ trunk/easyui/WebRoot/jsdom/ADS.js
@@ -16,29 +16,29 @@
 
 	function $() {
 		var elements = new Array;
-		
-		//查找作为参数的所有元素
+
+		// 查找作为参数的所有元素
 		for ( var int = 0; int < arguments.length; int++) {
-			
+
 			var element = arguments[i];
-			
-			if(typeof element = "string") {
-				
+
+			if (typeof element == "string") {
+
 				element = document.getElementById(element);
-				
+
 			}
-			//如果只提供一个参数，则立即返回这个元素
-			if(arguments.length == 1) {
-				
+			// 如果只提供一个参数，则立即返回这个元素
+			if (arguments.length == 1) {
+
 				return element;
-				
+
 			}
-			
-			//否则，将它添加到数组中
+
+			// 否则，将它添加到数组中
 			elements.push(element);
-			
+
 		}
-		
+
 		return elements;
 
 	}
@@ -46,35 +46,50 @@
 
 	function addEvent(node, type, listener) {
 
-		if(!isCompatible()) {
+		if (!isCompatible()) {
 			return false;
 		}
-		if(!(node = $(node))) {
+		if (!(node = $(node))) {
 			return false;
 		}
-		if(node.addEventListener) {
-			//w3c方法
-			node.addEventListener(type,listener,false);
-			
+		if (node.addEventListener) {
+			// w3c方法
+			node.addEventListener(type, listener, false);
+
 			return true;
-			
-		} else if(node.attachEvent) {
-			//MISE方法
+
+		} else if (node.attachEvent) {
+			// MISE方法
 			node['e' + type + listener] = listener;
-			note[type + listener] =  function () {
+			note[type + listener] = function() {
 				node['e' + type + listener](window.event);
 			};
-			node.atachEvent('on' + type, node[type+listener]);
+			node.atachEvent('on' + type, node[type + listener]);
 			return true;
 		}
-		//两种方法都不具备，返回false
+		// 两种方法都不具备，返回false
 		return false;
-		
+
 	}
 	;
 	window['ADS']['addEvent'] = addEvent;
 
+	// 移除事件
 	function removeEvent(node, type, listener) {
+		if (!(node = $(node))) {
+			return false;
+		}
+		if (node.removeEventListener) {
+			// w3c
+			node.removeEventListener(type, listener, false);
+			return true;
+		} else if (node.detachEvent) {
+			// msie
+			node.detachEvent("on" + type, node[type + listener]);
+			node[type + listener] = null;
+			return true;
+		}
+		return false;
 
 	}
 	;
@@ -108,14 +123,57 @@
 	}
 	;
 	window['ADS']['prependChild'] = prependChild;
-	
+
 	function exampleLibraryMethod(obj) {
-		if(!(obj = $(obj))) {
+		if (!(obj = $(obj))) {
 			return false;
 		}
 	}
-	
-	window['ADS']['exampleLibraryMethod'] = exampleLibraryMethod;
-	
 
+	window['ADS']['exampleLibraryMethod'] = exampleLibraryMethod;
+
+	//根据className获得对应的数组
+	function getElementsByClassName(className, tag, parent) {
+		parent = parent || document;
+		if (!(parent = $(parent))) {
+			return false;
+		}
+
+		// 查找所有匹配的标签
+		var allTags = (tag == "*" && parent.all) ? parent.all : parent
+				.getElementsByTagName(tag);
+		alert(allTags);
+		var matchingElements = new Array;
+
+		// 创建一个正则表达式，来判断className是否正确
+		className = className.replace(/\-/g, "\\-");
+		var regex = new RegExp("(^|\\s)" + className + "(\\s|$)");
+		var element;
+		// 检查每一个标签元素，如果符合，放入数组。
+		for ( var int = 0; int < allTags.length; int++) {
+			element = allTags[i];
+			if (regex.test(element.className)) {
+				matchingElements.push(element);
+			}
+		}
+
+		return matchingElements;
+
+	}
+	
+	window['ADS']['getElementsByClassName'] = getElementsByClassName;
+	
+	function toggleDisplay(node ,value) {
+		if(!(node = $(node))) {
+			return false;
+		}
+		if(node.style.display != "none") {
+			node.style.display = "none";
+		} else {
+			node.style.display = value || "";
+		}
+		return true;
+	}
+	window["ADS"]["toggleDisplay"] = toggleDisplay; 
+	
 })();
