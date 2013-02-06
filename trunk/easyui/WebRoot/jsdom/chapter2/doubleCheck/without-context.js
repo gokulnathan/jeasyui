e6452593d9cc3fe56ab9a5fe15d96df79d9680eb
sgq1980@gmail.com <sgq1980@gmail.com@4e47c4b2-cd81-ba98-324a-050b598eeac0>
2013-02-06 14:28:12 +0000

    js 学习

--- trunk/easyui/WebRoot/jsdom/chapter2/doubleCheck/without-context.js
+++ trunk/easyui/WebRoot/jsdom/chapter2/doubleCheck/without-context.js
@@ -13,3 +13,22 @@ initPage() {
     }
 }
 ADS.addEvent(window,'load',initPage);
+/*
+function doubleCheck() {
+	this.message = 'are you sure you want to leave?';
+};
+
+doubleCheck.prototype.sayGoodbye = function() {
+	return confirm(this.message);
+};
+
+initPage() {
+	var clickedLink = new doubleCheck();
+	var links = document.getElementsByTagName("a");
+	for ( var int = 0; int < links.length; int++) {
+		ADS.addEvent(links[i], 'click', clickedLink.sayGoodbye);
+	}
+};
+
+ADS.addEvent(window,'load', initPage);
+*/
